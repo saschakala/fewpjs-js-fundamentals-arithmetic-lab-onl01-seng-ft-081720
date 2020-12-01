2632a6ebbc20b88b6c2cd46cbbb02e035194ed4b
“saschakala” <mxsaschakala@gmail.com>
2020-11-30 21:50:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2 
+
+const random = Math.ceil(Math.random() * 10)
+
+const mod = 20 % 8 
+
+const max = (1, 2, 3, 4, 20)
